446c11aef606f975afc37223385faaeba859eba5
thewinguist <info@thewinguist.pl>
2023-06-08 22:39:53 +0200

    Learn about typeof + install Code Runner in VSC, lol

---
+++ numbers.js
@@ -0,0 +1,9 @@
+const myInt = 5;
+const myFloat = 6.667;
+console.log(typeof myInt);
+console.log(typeof myFloat);
+/*
+I installed Node.js just to see these
+comfy in VSC, not Firefox dev tools, lol
+https://linuxhint.com/javascript-visual-studio-code/
+*/
