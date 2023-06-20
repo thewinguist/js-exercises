6ff0a0487aeb506fd228e54a6d42099dc59e79ce
thewinguist <info@thewinguist.pl>
2023-06-21 01:18:49 +0200

    Oops, forgot to save file locally

--- numbers.js
+++ numbers.js
@@ -47,5 +47,11 @@ section.appendChild(para2);
 // Getting a random number between two values
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
-  }
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
   
